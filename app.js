1abfbad93854dfad7a8566143ff8f867bba67898
Caroline Olby <carolineolby@Carolines-MBP.lexicon.edu.local>
2020-02-27 11:43:58 +0100

    webbtjänster och JSON

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
-
+const Datastore = require('nedb-promise')
+const insults = new Datastore({filename:'insults.db', autoload:true})
 const app = express()
 
-app.get('/hello', (req,res) => {
-    res.send({ hello: 'world' });
+// app.get('/hello', (req,res) => {
+//     res.send({ hello: 'world' });
+// })
+
+app.get('/insults/:severity', async (req, res) => {
+    const documents = await insults.find({ severity: parseInt(req.params.severity) })
+    if (documents.length > 0){
+        res.json({ "documents": documents })
+    } else {
+        res.status(404);
+        res.send({ error: 'Not found' });
+    }
 })
 
 function startServer(){
